35bc8541f2ad09ac59989c5f269b743e2839d0a1
Nordine Bittich <nordine@bittich.be>
2023-04-29 15:32:29 +0200

    add permissions & test sending sms

--- app/src/main/java/tech/artcoded/smsgateway/MainActivity.kt
+++ app/src/main/java/tech/artcoded/smsgateway/MainActivity.kt
@@ -1,7 +1,9 @@
 package tech.artcoded.smsgateway
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.telephony.SmsManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -20,101 +22,144 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.MultiplePermissionsState
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import kotlinx.coroutines.*
 import tech.artcoded.smsgateway.ui.theme.SmsGatewayTheme
 
+@OptIn(ExperimentalPermissionsApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         title = "Sms Gateway"
+
+
         setContent {
+            val multiplePermissionsState = rememberMultiplePermissionsState(
+                listOf(
+                    Manifest.permission.SEND_SMS,
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.READ_PHONE_STATE,
+                )
+            )
+
             SmsGatewayTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    SmsGatewayMainPage("")
+                    SmsGatewayMainPage("", multiplePermissionsState)
                 }
             }
-        }
-    }
 
+
+        }
+
+
+    }
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun SmsGatewayMainPage(defaultEndpoint: String, modifier: Modifier = Modifier) {
-    var endpoint by remember {
-        mutableStateOf(defaultEndpoint)
-    }
-    var apiKey by remember { mutableStateOf("") }
-    var started by remember { mutableStateOf(false) }
-    var logTraces by remember { mutableStateOf("Not started...") }
-    Box(
-        modifier = Modifier
-            .padding(2.dp)
-            .fillMaxWidth()
-    ) {
-        Scaffold {
-            Column(
-                verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.Start
-            ) {
-                Spacer(modifier = Modifier.height(15.dp))
-                TextField(value = endpoint, modifier = Modifier.fillMaxWidth(), enabled = !started,
-
-                    label = { Text(text = "Endpoint") }, onValueChange = { newText ->
-                        endpoint = newText
-                    })
-                Spacer(modifier = Modifier.height(5.dp))
-                TextField(value = apiKey,
-                    modifier = Modifier.fillMaxWidth(),
-                    enabled = !started,
-                    label = { Text(text = "Api Key") },
-                    onValueChange = { newText ->
-                        apiKey = newText
-                    })
-                Button(
-                    onClick = {
-                        started = !started
-                        logTraces += if (started) "\nStarted..." else {
-                            "\nStopped..."
-                        }
-                    }, modifier = Modifier.fillMaxWidth(),
-                    shape = RoundedCornerShape(5.dp)
-                ) {
-                    Text(
-                        if (started) "Stop" else "Start", modifier = modifier
-                    )
+fun SmsGatewayMainPage(
+    defaultEndpoint: String,
+    multiplePermissionsState: MultiplePermissionsState,
+    modifier: Modifier = Modifier
+) {
+    if (!multiplePermissionsState.allPermissionsGranted) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(15.dp)
+        ) {
+            Button(onClick = { multiplePermissionsState.launchMultiplePermissionRequest() }) {
+                Text("Request permissions")
+            }
+        }
+    } else {
+        // useful spaghetti code starts here
+        val androidCtx = LocalContext.current
+        val smsManager = androidCtx.getSystemService(SmsManager::class.java)
+        var endpoint by remember {
+            mutableStateOf(defaultEndpoint)
+        }
+        var apiKey by remember { mutableStateOf("") }
+        var started by remember { mutableStateOf(false) }
+        var logTraces by remember { mutableStateOf("Not started...") }
+        val coroutineScope = rememberCoroutineScope()
+        val startStopToggle: () -> Unit = {
+            coroutineScope.launch {
+                started = !started
+                logTraces += if (started) "\nStarted..." else {
+                    "\nStopped..."
                 }
-                Spacer(modifier = Modifier.height(5.dp))
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(color = Color.LightGray, shape = RoundedCornerShape(5.dp))
+                while (started) {
+                    logTraces += "\n sending message..."
+                    smsManager.sendTextMessage("+32XXXXXXX", null, "hello", null, null)
+                    delay(6000)
+                }
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(2.dp)
+                .fillMaxWidth()
+        ) {
+            Scaffold {
+                Column(
+                    verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.Start
                 ) {
-                    Text(
-                        text = logTraces,
+                    Spacer(modifier = Modifier.height(15.dp))
+                    TextField(value = endpoint,
+                        modifier = Modifier.fillMaxWidth(),
+                        enabled = !started,
+                        label = { Text(text = "Endpoint") },
+                        onValueChange = { newText ->
+                            endpoint = newText
+                        })
+                    Spacer(modifier = Modifier.height(5.dp))
+                    TextField(value = apiKey,
+                        modifier = Modifier.fillMaxWidth(),
+                        enabled = !started,
+                        label = { Text(text = "Api Key") },
+                        onValueChange = { newText ->
+                            apiKey = newText
+                        })
+                    Button(
+                        onClick = startStopToggle,
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(5.dp)
+                    ) {
+                        Text(
+                            if (started) "Stop" else "Start", modifier = modifier
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Box(
                         modifier = Modifier
-                            .padding(5.dp)
-                            .verticalScroll(rememberScrollState(0)),
-                    )
+                            .fillMaxSize()
+                            .background(color = Color.LightGray, shape = RoundedCornerShape(5.dp))
+                    ) {
+                        Text(
+                            text = logTraces,
+                            modifier = Modifier
+                                .padding(5.dp)
+                                .verticalScroll(rememberScrollState(0)),
+                        )
+                    }
                 }
             }
+
         }
-
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun SmsGatewayMainPagePreview() {
-    SmsGatewayTheme {
-        SmsGatewayMainPage(defaultEndpoint = "http://google.com")
-    }
 }
