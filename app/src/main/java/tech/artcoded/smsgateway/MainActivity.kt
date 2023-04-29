5f202251b6f6744da9f718e07aebad8b88492c74
Nordine Bittich <nordine@bittich.be>
2023-04-29 11:52:54 +0200

    a bit of cleanup

--- app/src/main/java/tech/artcoded/smsgateway/MainActivity.kt
+++ app/src/main/java/tech/artcoded/smsgateway/MainActivity.kt
@@ -4,13 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clipScrollableContainer
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -29,9 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import tech.artcoded.smsgateway.ui.theme.SmsGatewayTheme
@@ -46,8 +38,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
-
+                    SmsGatewayMainPage("")
                 }
             }
         }
@@ -57,9 +48,9 @@ class MainActivity : ComponentActivity() {
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(endpoint: String, modifier: Modifier = Modifier) {
+fun SmsGatewayMainPage(defaultEndpoint: String, modifier: Modifier = Modifier) {
     var endpoint by remember {
-        mutableStateOf(endpoint)
+        mutableStateOf(defaultEndpoint)
     }
     var apiKey by remember { mutableStateOf("") }
     var started by remember { mutableStateOf(false) }
@@ -121,9 +112,8 @@ fun Greeting(endpoint: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-
+fun SmsGatewayMainPagePreview() {
     SmsGatewayTheme {
-        Greeting(endpoint = "http://google.com")
+        SmsGatewayMainPage(defaultEndpoint = "http://google.com")
     }
 }
