5ea8668558560885e620885c634e412692db92ea
Nordine Bittich <nordine@bittich.be>
2023-05-01 17:49:26 +0200

    lgtm

--- app/src/main/java/tech/artcoded/smsgateway/MainActivity.kt
+++ app/src/main/java/tech/artcoded/smsgateway/MainActivity.kt
@@ -36,7 +36,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startForegroundService
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.MultiplePermissionsState
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
--- app/src/main/java/tech/artcoded/smsgateway/Utils.kt
+++ app/src/main/java/tech/artcoded/smsgateway/Utils.kt
@@ -8,23 +8,23 @@ import androidx.security.crypto.MasterKey
 import java.security.GeneralSecurityException
 import java.security.KeyStore
 
-private const val SHARED_PREFS_FILENAME = "sms_gateway_secret_shared_prefs";
+private const val SHARED_PREFS_FILENAME = "sms_gateway_secret_shared_prefs"
 
 object Utils {
 
     fun createEncryptedSharedPrefDestructively(context: Context): SharedPreferences {
         return try {
-            createEncryptedSharedPref(SHARED_PREFS_FILENAME, context)
+            createEncryptedSharedPref(context)
         } catch (e: GeneralSecurityException) {
             deleteMasterKeyEntry()
-            deleteExistingPref(SHARED_PREFS_FILENAME, context)
-            createEncryptedSharedPref(SHARED_PREFS_FILENAME, context)
+            deleteExistingPref(context)
+            createEncryptedSharedPref(context)
         }
     }
 
     @SuppressLint("ApplySharedPref")
-    private fun deleteExistingPref(fileName: String, context: Context) {
-        context.deleteSharedPreferences(fileName)
+    private fun deleteExistingPref( context: Context) {
+        context.deleteSharedPreferences(SHARED_PREFS_FILENAME)
     }
 
     private fun deleteMasterKeyEntry() {
@@ -34,14 +34,14 @@ object Utils {
         }
     }
 
-    private fun createEncryptedSharedPref(fileName: String, context: Context): SharedPreferences {
+    private fun createEncryptedSharedPref(context: Context): SharedPreferences {
         val masterKey = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
             .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
             .build()
 
         return EncryptedSharedPreferences.create(
             context,
-            fileName,
+            SHARED_PREFS_FILENAME,
             masterKey,
             EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
             EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
